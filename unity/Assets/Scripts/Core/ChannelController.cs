f14c0f989d018e6bd50cd3af6c0c706f6234922f
jbat100 <dev.jonathan.thorpe@gmail.com>
2017-09-26 21:01:02 +0200

    ChannelController implementation work

--- unity/Assets/Scripts/Core/ChannelController.cs
+++ unity/Assets/Scripts/Core/ChannelController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
+using com.spacepuppy.Collections;
 
 namespace Sonosthesia
 {
@@ -14,6 +15,25 @@ namespace Sonosthesia
 		OUTGOING,
 		DUPLEX
 	}
+
+    [Serializable]
+    public class ChannelParameterDescription
+    {
+        public string key;
+        public float defaultValue;
+        public float maxValue;
+        public float minValue;
+        public int dimensions;
+
+        public ChannelParameterDescription(string _key, float _defaultValue = 0f, float _maxValue = 1f, float _minValue = 0f, int _dimensions = 1)
+        {
+            key = _key;
+            defaultValue = _defaultValue;
+            maxValue = _maxValue;
+            minValue = _minValue;
+            dimensions = _dimensions;
+        }
+    }
 
     public class ChannelParameterSet
     {
@@ -48,6 +68,11 @@ namespace Sonosthesia
     public struct ChannelControllerStaticEventArgs
     {
         public ChannelParameterSet parameters;
+
+        public ChannelControllerStaticEventArgs(ChannelParameterSet _parameters)
+        {
+            parameters = _parameters;
+        }
     }
     
     public delegate void ChannelControllerStaticEventHandler(object sender, ChannelControllerStaticEventArgs e);
@@ -55,11 +80,18 @@ namespace Sonosthesia
     public struct ChannelControllerDynamicEventArgs
     {
         public ChannelInstance instance;
+
+        public ChannelControllerDynamicEventArgs(ChannelInstance _instance)
+        {
+            instance = _instance;
+        }
     }
 
     public delegate void ChannelControllerDynamicEventHandler(object sender, ChannelControllerDynamicEventArgs e);
 
     public class ChannelController : MonoBehaviour {
+
+        private static ObjectCachePool<ChannelInstance> instanceCache = new ObjectCachePool<ChannelInstance>(1000); 
 
         public string identifier;
 
@@ -81,12 +113,25 @@ namespace Sonosthesia
         public event ChannelControllerDynamicEventHandler ControlInstanceEvent;
         public event ChannelControllerDynamicEventHandler DestroyInstanceEvent;
 
+        // store for return to instanceCache on LateUpdate
+        private List<ChannelInstance> deadInstances = new List<ChannelInstance>();
+
         private void Awake()
         {
             if (!componentController)
             {
                 componentController = GetComponentInParent<ComponentController>();
             }
+        }
+
+        private void LateUpdate()
+        {
+            foreach(ChannelInstance instance in deadInstances)
+            {
+                instanceCache.Release(instance);
+            }
+
+            instances.Clear();
         }
 
         public void ApplyIncomingMessage(ChannelMessage message)
@@ -98,14 +143,90 @@ namespace Sonosthesia
 
             if (message.key.instance != null)
             {
-
+                switch (message.type)
+                {
+                    case MessageType.Control:
+                        {
+                            ChannelInstance instance = EnsureChannelInstance(message.key.instance);
+                            ExtractParameterSet(instance.parameters, message);
+                            if (ControlInstanceEvent != null)
+                            {
+                                ControlInstanceEvent(this, new ChannelControllerDynamicEventArgs(instance));
+                            }
+                        }
+                        break;
+                    case MessageType.Create:
+                        {
+                            ChannelInstance instance = EnsureChannelInstance(message.key.instance);
+                            ExtractParameterSet(instance.parameters, message);
+                            if (CreateInstanceEvent != null)
+                            {
+                                CreateInstanceEvent(this, new ChannelControllerDynamicEventArgs(instance));
+                            }
+                        }
+                        break;
+                    case MessageType.Destroy:
+                        {
+                            ChannelInstance instance = CleanChannelInstance(message.key.instance);
+                            ExtractParameterSet(instance.parameters, message);
+                            if (DestroyInstanceEvent != null)
+                            {
+                                DestroyInstanceEvent(this, new ChannelControllerDynamicEventArgs(instance));
+                            }
+                        }
+                        break;
+                    default:
+                        Debug.LogWarning("unepexted message type: " + message.type);
+                        break;
+                }
             }
-
+            else
+            {
+                switch (message.type)
+                {
+                    case MessageType.Control:
+                        {
+                            ExtractParameterSet(staticParameters, message);
+                            if (StaticControlEvent != null)
+                            {
+                                StaticControlEvent(this, new ChannelControllerStaticEventArgs(staticParameters));
+                            }
+                        }
+                        break;
+                }
+            }
         }
 
         public void SendOutgoingMessage(ChannelMessage message)
         {
 
+        }
+
+        private void ExtractParameterSet(ChannelParameterSet parameterSet, ChannelMessage message)
+        {
+            parameterSet.Apply(message.parameters);
+        }
+
+        private ChannelInstance EnsureChannelInstance(string identifier)
+        {
+            ChannelInstance instance = null;
+            if (!instances.TryGetValue(identifier, out instance))
+            {
+                instance = instanceCache.GetInstance();
+                instances[identifier] = instance;
+            }
+            return instance;
+        }
+
+        private ChannelInstance CleanChannelInstance(string identifier)
+        {
+            ChannelInstance instance = null;
+            if (instances.TryGetValue(identifier, out instance))
+            {
+                deadInstances.Add(instance);
+                instances.Remove(identifier);
+            }
+            return instance;
         }
     }
     
